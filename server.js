026ea514fc3a2504713876b5f84574d39dc9b198
Stanley Lau <stanleyyylau@gmail.com>
2016-08-07 10:47:42 +0000

    finish the project with all functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var date = require("datejs");
 
 
 app.use(bodyParser.json());
@@ -8,14 +9,43 @@ app.use(bodyParser.json());
 
 app.get('/:id', function (req, res) {
   var id = req.params.id;
-  var unixTimestamp = new Date(id*1000);
   var monthes = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-  var convertedTime = {
+  var convertedTime = {};
+  var unixTimestamp;
+  //if id is a date string
+  if (id.match(/[a-z]/i)) {
+        if(Date.parse(id) === null){
+            return res.json({
+                unix:null,
+                natural:null
+            });
+        }
+        var unixtime = Date.parse(id).getTime()/1000;
+        unixTimestamp = new Date(unixtime*1000);
+        convertedTime = {
+        unix: Number(unixtime),
+        natural:monthes[unixTimestamp.getMonth()] + " " + unixTimestamp.getDate() + ", " + unixTimestamp.getFullYear()
+        }
+        res.json(convertedTime);
+    }
+    else{
+        if(Date.parse(id) === null){
+            return res.json({
+                unix:null,
+                natural:null
+            });
+        }
+      unixTimestamp = new Date(id*1000);
+      convertedTime = {
       unix: Number(id),
       natural:monthes[unixTimestamp.getMonth()] + " " + unixTimestamp.getDate() + ", " + unixTimestamp.getFullYear()
-  }
-  res.json(convertedTime);
+      }
+    res.json(convertedTime);
+    }
 });
+  
+  
+  
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
