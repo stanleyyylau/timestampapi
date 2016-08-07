2d90a0f2457a3532111fcdfb18b90ea91fd53553
Stanley Lau <stanleyyylau@gmail.com>
2016-08-07 09:51:27 +0000

    can now convert unix timestamp to natual date

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+app.use(bodyParser.json());
+
+
+app.get('/:id', function (req, res) {
+  var id = req.params.id;
+  var unixTimestamp = new Date(id*1000);
+  var monthes = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  var convertedTime = {
+      unix: Number(id),
+      natural:monthes[unixTimestamp.getMonth()] + " " + unixTimestamp.getDate() + ", " + unixTimestamp.getFullYear()
+  }
+  res.json(convertedTime);
 });
 
 app.listen(8080, function () {
